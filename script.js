fa4f6f8e768fb2c6cf4763c1c5e1579a673f28f1
Yunseong Kim <yunseongkim7@gmail.com>
2025-12-21 00:07:44 -0500

    inital

---
+++ script.js
@@ -0,0 +1,14 @@
+const pageFlip = new St.PageFlip(
+    document.getElementById("book"),
+    {
+      width: 350,
+      height: 500,
+      size: "stretch",
+      maxShadowOpacity: 0.5,
+      showCover: true,
+      mobileScrollSupport: false
+    }
+  );
+  
+  pageFlip.loadFromHTML(document.querySelectorAll(".page"));
+  
